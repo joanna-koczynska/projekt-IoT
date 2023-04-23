b3226446216f7739f0cb85dea1fca9ed20dbc738
joanna-koczynska <joannakoczynska@gmail.com>
2023-04-23 14:32:33 +0200

    Dodaj pliki projektów.

---
+++ projekt IoT/program.cs
@@ -0,0 +1,16 @@
+﻿//using devicedemo.Device;
+//using Microsoft.Azure.Devices.Client;
+
+//string deviceConnectionString = "HostName=IoT-centrum.azure-devices.net;DeviceId=Device1;SharedAccessKey=kFvAwD1b2RVkGhoPbH5ESa4a/Htxfn2PTnYPz+a85Ns=";
+
+//using var deviceClient = DeviceClient.CreateFromConnectionString(deviceConnectionString, TransportType.Mqtt);
+//await deviceClient.OpenAsync();
+//var device = new VirtualDevice(deviceClient);
+//Console.WriteLine("Connection success");
+//await device.InitializeHandlers();
+//await device.UpdateTwinAsync();
+
+//await device.SendMessages(10, 1000);
+
+//Console.WriteLine("Finished! Press key to close...");
+//Console.ReadLine();
