1da81f4c1db8f833dfb24242db3bca484e34d193
joanna-koczynska <joannakoczynska@gmail.com>
2023-05-07 15:08:14 +0200

    telemetry D2C Messages

---
+++ deviceLibrary/functions.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Azure.Devices.Client;
+using Microsoft.Azure.Devices.Shared;
+using Newtonsoft.Json;
+using Opc.UaFx.Client;
+using Opc.UaFx;
+using System.Net.Mime;
+using System.Text;
+using System.Xml;
+using Microsoft.VisualBasic;
+using System.Threading.Tasks;
+
+
+
+
+namespace deviceLib
+{
+    public class functions
+    {
+        private readonly DeviceClient client;
+
+        public functions(DeviceClient deviceClient)
+        {
+            this.client = deviceClient;
+        }
+
+        #region Sending Messages
+
+        public async Task SendMessages(OpcValue ProductionStatus, OpcValue WorkorderId, OpcValue GoodCount, OpcValue BadCount, OpcValue Temperature)
+        {
+            var data = new
+            {
+                ProductionStatus = ProductionStatus,
+                WorkorderId = WorkorderId,
+                GoodCount = GoodCount,
+                BadCount = BadCount,
+                Temperature = Temperature,
+            };
+
+            var dataString = JsonConvert.SerializeObject(data);
+
+            Message eventMessage = new Message(Encoding.UTF8.GetBytes(dataString));
+            eventMessage.ContentType = MediaTypeNames.Application.Json;
+            eventMessage.ContentEncoding = "utf-8";
+            await client.SendEventAsync(eventMessage);
+        }
+
+
+
+        #endregion Sending Messages
+
+
+        #region Device Twin
+
+        public async Task UpdateTwinAsync()
+        {
+            var twin = await client.GetTwinAsync();
+
+            Console.WriteLine($"\nInitial twin value received: \n{JsonConvert.SerializeObject(twin, Formatting.Indented)}");
+            Console.WriteLine();
+
+            var reportedProperties = new TwinCollection();
+            reportedProperties["DateTimeLastAppLaunch"] = DateTime.Now;
+
+            await client.UpdateReportedPropertiesAsync(reportedProperties);
+        }
+
+        private async Task OnDesiredPropertyChanged(TwinCollection desiredProperties, object userContext)
+        {
+            Console.WriteLine($"\tDesired property change:\n\t{JsonConvert.SerializeObject(desiredProperties)}");
+            Console.WriteLine("\tSending current time as reported property");
+            TwinCollection reportedProperties = new TwinCollection();
+            reportedProperties["DateTimeLastDesiredPropertyChangeReceived"] = DateTime.Now;
+
+            await client.UpdateReportedPropertiesAsync(reportedProperties).ConfigureAwait(false);
+        }
+
+        #endregion Device Twin
+
+    }
+
+}
+
--- projekt IoT/program.cs
+++ projekt IoT/program.cs
@@ -1,18 +1,50 @@
 ﻿using deviceLib;
 using Microsoft.Azure.Devices.Client;
+using Opc.UaFx.Client;
+using Opc.UaFx;
 
-string deviceConnectionString = "HostName=IoT-centrum.azure-devices.net;DeviceId=Device1;SharedAccessKey=kFvAwD1b2RVkGhoPbH5ESa4a/Htxfn2PTnYPz+a85Ns=";
+    string deviceConnectionString = "HostName=IoT-centrum.azure-devices.net;DeviceId=Device1;SharedAccessKey=kFvAwD1b2RVkGhoPbH5ESa4a/Htxfn2PTnYPz+a85Ns=";
 
 using var deviceClient = DeviceClient.CreateFromConnectionString(deviceConnectionString, TransportType.Mqtt);
 await deviceClient.OpenAsync();
-var device = new device(deviceClient);
+    var device = new functions(deviceClient);
 
-/*await device.InitializeHandlers();*/
+    Console.WriteLine($"Connection success!");
+
+    using (var client = new OpcClient("opc.tcp://localhost:4840/"))
+    {
+        client.Connect();
 
 
-Console.WriteLine($"Connection success!");
-/*
-await device.SendMessages(10, 1000);*/
+        OpcValue WorkorderId = client.ReadNode("ns=2;s=Device 1/WorkorderId");
+        OpcValue ProductionStatus = client.ReadNode("ns=2;s=Device 1/ProductionStatus");
+        OpcValue GoodCount = client.ReadNode("ns=2;s=Device 1/GoodCount");
+        OpcValue BadCount = client.ReadNode("ns=2;s=Device 1/BadCount");
+        OpcValue Temperature = client.ReadNode("ns=2;s=Device 1/Temperature");
+        OpcValue ProductionRate = client.ReadNode("ns=2;s=Device 1/ProductionRate");
 
-Console.WriteLine("Finished! Press Enter to close...");
-Console.ReadLine();
+
+
+        OpcValue DeviceErrors = client.ReadNode("ns=2;s=Device 1/DeviceError");
+        uint errorValue = Convert.ToUInt32(DeviceErrors.Value);
+        Errors errors = (Errors)errorValue;
+
+    await device.SendMessages(ProductionStatus, WorkorderId, GoodCount, BadCount, Temperature);
+    }
+
+
+[Flags]
+public enum Errors
+{
+    a = 0,
+    b = 1,
+    c = 2,
+    d = 4,
+    e = 8
+};
+
+
+
+
+
+
